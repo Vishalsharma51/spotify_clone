956aece0960562c6c51fa969e5ddc63f29652b9f
Vishalsharma51 <vishisharma51@gmail.com>
2024-09-28 14:02:16 +0530

    commited

---
+++ script84.js
@@ -0,0 +1,149 @@
+console.log("hiii");
+async function getsongs() {
+   let a = await fetch("http://127.0.0.1:5500/songs/")
+   let response = await a.text();
+   console.log(response)
+   let div = document.createElement("div")
+   div.innerHTML = response;
+   let as = div.getElementsByTagName("a")
+   let songs = []
+   for (let index = 0; index < as.length; index++) {
+      const element = as[index];
+      if (element.href.endsWith(".mp3")) {
+         songs.push(element.href)
+      }
+
+   }
+   return songs;
+}
+
+async function main() {
+   let songs = await getsongs();
+   console.log(songs);
+
+   let songUL = document.querySelector(".songlist ul");
+   for (const song of songs) {
+      songUL.innerHTML = songUL.innerHTML + song;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+   }
+   
+
+   var audio = new Audio(songs[0]);
+   //  audio.play();
+
+   audio.addEventListener("loadeddata",()=>{
+      console.log(audio.duration,audio.currentSrc,audio.currentTime)
+   })
+} 
+main()
